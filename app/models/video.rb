cd7bc72771077075fb820e2a6e4e8090229cd9c3
taeinha <taeinha@gmail.com>
2019-12-11 18:00:04 -0500

    Push updates to video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -13,10 +13,25 @@
 
 class Video < ApplicationRecord
 
+  validates :title, :description, :view_count, :uploader_id, presence: true
+
+  validate :ensure_video
+  after_initialize :ensure_view_count
+
   belongs_to :uploader,
     primary_key: :id,
     foreign_key: :uploader_id,
     class_name: :User
 
   has_one_attached :video
+
+  def ensure_video
+    unless self.video.attached?
+      errors[:video] << "Video must be attached!"
+    end
+  end
+
+  def ensure_view_count
+    self.view_count ||= 0
+  end
 end
